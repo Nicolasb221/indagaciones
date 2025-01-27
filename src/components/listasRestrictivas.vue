27751da2a5f9310d3182c3d841db1643601d257f
Nicolás Alberto Bravo Silva <88752910+Nicolasb221@users.noreply.github.com>
2025-01-27 22:27:22 +0000

    u1

--- src/components/listasRestrictivas.vue
+++ src/components/listasRestrictivas.vue
@@ -397,8 +397,12 @@
     margin: 20px;
     padding: 20px;
     border-radius: 15px;
-    justify-content: space-evenly;
     border: 8px solid var(--color-azul-morado);
+    background: linear-gradient(135deg, var(--color-gradiente-inicio), var(--color-gradiente-fin));
+    backdrop-filter: blur(10px);
+    box-shadow: 0 10px 20px var(--color-azul-morado);
+    background-color: #DBDBDB;
+    justify-content: space-evenly;
   }
   .container-bot{
     display: flex;
