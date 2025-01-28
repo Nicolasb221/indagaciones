912fbc5f6af0e46592a416dd485a52e7d35e8d96
Nicolás Alberto Bravo Silva <88752910+Nicolasb221@users.noreply.github.com>
2025-01-28 20:56:31 +0000

    u1

--- src/components/listasRestrictivas.vue
+++ src/components/listasRestrictivas.vue
@@ -459,7 +459,7 @@
   .container-datos-i{
     display:flex;
     max-width: 1200px;
-    min-width: 838px;
+    min-width: 848px;
     flex-direction: column;
     margin: 20px;
     padding: 20px;
