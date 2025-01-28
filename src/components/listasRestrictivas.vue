778c7ed15e5af559336413234f17d3b0ba637220
Nicolás Alberto Bravo Silva <88752910+Nicolasb221@users.noreply.github.com>
2025-01-28 20:24:50 +0000

    u1

--- src/components/listasRestrictivas.vue
+++ src/components/listasRestrictivas.vue
@@ -37,7 +37,8 @@
               <div class="botonbusqueda">Informa</div>
               <fa :icon="['fas', 'circle-check']" style="color: var(--color-azul-kp);" v-show="datosAlmacenados.flagCheckout[2]"/>
             </div>
-            <div class="container-clases-2"><div class="container-icon"><fa :icon="['fas', 'circle-question']" style="color: var(--color-azul-kp);" /></div><div class="titulo-clase-2">Tiene vinculaciones societarias?</div></div>
+            <div class="container-clases-2"><div class="container-icon"><fa :icon="['fas', 'circle-question']" style="color: var(--color-azul-kp);" /></div><div class="titulo-clase-2">¿Tiene vinculaciones societarias?</div></div>
+            <div class="container-clases-2"><fa :icon="['fas', 'check']"; style="color: var(--color-azul-kp);" /></div>
           </div>
           <div class="container-bot">
             <div class="container-clases"><div class="container-icon"><fa :icon="['fas', 'user-shield']" style="color: var(--color-azul-kp);" /></div><div class="titulo-clase">INTEGRIDAD</div></div>
