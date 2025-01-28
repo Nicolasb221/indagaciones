548cfc312e4d8c1f530bdf48f6d1332311b2c6f0
Nicolás Alberto Bravo Silva <88752910+Nicolasb221@users.noreply.github.com>
2025-01-28 00:52:35 +0000

    u1

--- src/components/listasRestrictivas.vue
+++ src/components/listasRestrictivas.vue
@@ -424,7 +424,7 @@
     background-color: transparent;
     border: none;
     cursor:pointer;
-    font-size: 28px;
+    font-size: 5px;
     margin:5px;
     font-family: 'Bubbler One', sans-serif;
     transition: color .3s ease-in, font-weight .3s ease-in, font-size .3s ease-in;
